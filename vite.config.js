53d74bbd309d603f19af372d0c079af24db307af
Danila Meda <danila.meda@acgnn.ru>
2025-05-12 20:47:27 +0300

    finish

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  base: "./",
+  plugins: [vue(), tailwindcss(), viteSingleFile()],
+  build: {
+    target: "esnext",
+    assetsInlineLimit: 100000000, // Инлайнит все ресурсы
+  },
 });
